2d78317aa6b07290273f1c873bb23f40c0ee349e
neeraj kumar sharma <neerajk@allendigital.in>
2022-11-25 21:33:13 +0530

    pankaj msg

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "hello neeraj";
+        return "hello pankaj";
     }
 
 }
