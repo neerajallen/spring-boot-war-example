9e5461a42eaf6314e463676f0dcbab43543f727e
neeraj kumar sharma <neerajk@allendigital.in>
2022-11-26 10:29:48 +0530

    add sankalp string in code

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "hello pankaj gupta";
+        return "sankalp";
     }
 
 }
