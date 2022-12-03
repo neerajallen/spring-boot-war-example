65b33533ecf8728a3bf06b585a770c2d72c088d0
neerajallen <100480612+neerajallen@users.noreply.github.com>
2022-12-03 19:06:40 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Saket Sharma";
+        return "devendra sharma";
     }
 
 }
